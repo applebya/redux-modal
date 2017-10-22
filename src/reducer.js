997178c2f3e8a83aa24a9e67d2bc09677764b642
Andrew Appleby <andrewappleby22@gmail.com>
2017-10-21 17:15:40 -0700

    Reducers use immutable methods, initialState now Imm.Map()

--- src/reducer.js
+++ src/reducer.js
@@ -1,30 +1,22 @@
 import { SHOW, HIDE, DESTROY } from "./actionTypes";
+import { Map } from "immutable";
 
-const initialState = {};
+const initialState = Map();
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case SHOW:
-      return {
-        ...state,
-        [action.payload.modal]: {
+      return state.set(
+        action.payload.modal,
+        Map({
           show: true,
           props: action.payload.props
-        }
-      };
+        })
+      );
     case HIDE:
-      return {
-        ...state,
-        [action.payload.modal]: {
-          ...state[action.payload.modal],
-          show: false
-        }
-      };
+      return state.setIn([action.payload.modal, "show"], false);
     case DESTROY:
-      return {
-        ...state,
-        [action.payload.modal]: undefined
-      };
+      return state.remove(action.payload.modal);
     default:
       return state;
   }
